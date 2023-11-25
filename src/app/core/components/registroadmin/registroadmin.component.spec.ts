6cac965a03640b29a8bc963578db105f287eb745
Oscar Tobar <o.tobar@uniandes.edu.co>
2023-11-25 08:46:49 -0500

    se corrigen bugs de pruebas

--- src/app/core/components/registroadmin/registroadmin.component.spec.ts
+++ src/app/core/components/registroadmin/registroadmin.component.spec.ts
@@ -1,23 +1,3 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegistroadminComponent } from './registroadmin.component';
-
-describe('RegistroadminComponent', () => {
-  let component: RegistroadminComponent;
-  let fixture: ComponentFixture<RegistroadminComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ RegistroadminComponent ]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(RegistroadminComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
