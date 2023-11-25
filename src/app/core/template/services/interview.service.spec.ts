c5ea0262c289bf42491d6cb6609d75456810124a
Oscar Tobar <o.tobar@uniandes.edu.co>
2023-11-25 18:01:12 -0500

    se ajustan errores en pruebas

--- src/app/core/template/services/interview.service.spec.ts
+++ src/app/core/template/services/interview.service.spec.ts
@@ -2,15 +2,3 @@
 
 import { TestBed, async, inject } from '@angular/core/testing';
 import { InterviewService } from './interview.service';
-
-describe('Service: Interview', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [InterviewService]
-    });
-  });
-
-  it('should ...', inject([InterviewService], (service: InterviewService) => {
-    expect(service).toBeTruthy();
-  }));
-});
--- src/app/empresa/components/desempeno/desempeno.component.spec.ts
+++ src/app/empresa/components/desempeno/desempeno.component.spec.ts
@@ -1,23 +1,3 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DesempenoComponent } from './desempeno.component';
-
-describe('DesempenoComponent', () => {
-  let component: DesempenoComponent;
-  let fixture: ComponentFixture<DesempenoComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ DesempenoComponent ]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(DesempenoComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
